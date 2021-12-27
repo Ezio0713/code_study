47afe6698395c7f81985f66167604eb0f6f101ea
qzx <915712276@qq.com>
2021-12-27 09:43:39 +0800

    Create change.java

---
+++ code/12.17/change.java
@@ -0,0 +1,14 @@
+/*交换两个数
+*/
+class change 
+{
+	public static void main(String[] args) 
+	{
+		int a = 16;
+		int b = 19;
+		int c = a;
+		    a = b;
+		    b = c;
+		System.out.println(a + "," + b);
+	}
+}
